bf3307ab466e20f50605f0eb871d19070fd6011d
Lluis <l.salord.quetglas@gmail.com>
2020-03-18 15:40:23 +0100

    Proceed in chunks, merge and save all data

--- historical_preprocess.py
+++ historical_preprocess.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import time
+import os
 
 VADER_COLUMNS = [
     'Negative',
@@ -90,64 +91,109 @@ def weight_mean(x, df, weight_col, offset=0):
     return np.average(x, weights=weights)
 
 
-def tweetsPreprocess(tweets_path, nrows=None):
+def tweetsPreprocess(tweets_path, nrows=None, chunksize=5e5, save_path='data/preprocess/twitter.csv', write_files=True):
     
     print("Loading raw file")
-    raw_df = pd.read_csv(tweets_path, sep=';', nrows=nrows)
-
-    print("Transforming timestamp to datetime format")
-    raw_df['timestamp'] = pd.to_datetime(raw_df['timestamp'], format='%Y-%m-%d %H:%M:%S+00')
-
-    raw_df = raw_df[
-        (raw_df['timestamp'].notnull())
-        & (raw_df['text'].notnull()) 
-    ]
-
-    print("Adding Sentiment")
-    SENTIMENT_COLUMNS = []
-    
-    raw_df = addVaderSentiment(raw_df)
-    SENTIMENT_COLUMNS += VADER_COLUMNS
-
-    raw_df = addTextBlobSentiment(raw_df)
-    SENTIMENT_COLUMNS += TEXTBLOB_COLUMNS
-
-    func_dict = dict(
-        zip(
-            ['replies', 'likes', 'retweets'],
-            [['sum','mean'],] * 3
-        )
+    raw_df_chunks = pd.read_csv(
+        tweets_path,
+        sep=';',
+        usecols=['timestamp', 'replies', 'likes', 'retweets', 'text'],
+        nrows=nrows,
+        chunksize=chunksize
     )
 
-    replies_mean = lambda x: weight_mean(x, raw_df, weight_col='replies', offset=0)
-    likes_mean = lambda x: weight_mean(x, raw_df, weight_col='likes', offset=0)
-    retweets_mean = lambda x: weight_mean(x, raw_df, weight_col='retweets', offset=0)
+    agg_list = []
+    saved_files = []
+    for i, raw_df in enumerate(raw_df_chunks):
+        print(f"Processing chunk {i}...")
+        print("Transforming timestamp to datetime format")
+        raw_df['timestamp'] = pd.to_datetime(raw_df['timestamp'], format='%Y-%m-%d %H:%M:%S+00')
 
-    func_dict.update(
-        dict(
+        raw_df = raw_df[
+            (raw_df['timestamp'].notnull())
+            & (raw_df['text'].notnull()) 
+        ]
+
+        print("Adding Sentiment")
+        SENTIMENT_COLUMNS = []
+        
+        raw_df = addVaderSentiment(raw_df)
+        SENTIMENT_COLUMNS += VADER_COLUMNS
+
+        raw_df = addTextBlobSentiment(raw_df)
+        SENTIMENT_COLUMNS += TEXTBLOB_COLUMNS
+
+        func_dict = dict(
             zip(
-                SENTIMENT_COLUMNS,
-                [['mean', replies_mean, likes_mean, retweets_mean],] * len(SENTIMENT_COLUMNS)
+                ['replies', 'likes', 'retweets'],
+                [['sum','mean'],] * 3
             )
         )
-    )
 
-    print("Flooring timestamp")
-    raw_df['timestamp'] = raw_df['timestamp'].dt.floor('min')
+        replies_mean = lambda x: weight_mean(x, raw_df, weight_col='replies', offset=0)
+        likes_mean = lambda x: weight_mean(x, raw_df, weight_col='likes', offset=0)
+        retweets_mean = lambda x: weight_mean(x, raw_df, weight_col='retweets', offset=0)
 
-    columns = ['replies', 'likes', 'retweets'] 
-    columns += SENTIMENT_COLUMNS
+        func_dict.update(
+            dict(
+                zip(
+                    SENTIMENT_COLUMNS,
+                    [['mean', replies_mean, likes_mean, retweets_mean],] * len(SENTIMENT_COLUMNS)
+                )
+            )
+        )
 
-    print("Aggregating by timestamp")
-    agg_df = raw_df.groupby(['timestamp'])[columns].agg(func_dict)
-    
-    agg_df.columns = list(
-        agg_df.columns.to_frame()
-            .replace('<lambda_0>', 'replies_mean')
-            .replace('<lambda_1>', 'likes_mean')
-            .replace('<lambda_2>', 'retweets_mean') 
-            .agg('_'.join, axis=1)
-    )
+        print("Flooring timestamp")
+        raw_df['timestamp'] = raw_df['timestamp'].dt.floor('min')
+
+        columns = ['replies', 'likes', 'retweets'] 
+        columns += SENTIMENT_COLUMNS
+
+        print("Aggregating by timestamp")
+        agg_df = raw_df.groupby(['timestamp'])[columns].agg(func_dict)
+        
+        agg_df.columns = list(
+            agg_df.columns.to_frame()
+                .replace('<lambda_0>', 'replies_mean')
+                .replace('<lambda_1>', 'likes_mean')
+                .replace('<lambda_2>', 'retweets_mean') 
+                .agg('_'.join, axis=1)
+        )
+
+        if write_files:
+            partial_file = os.path.splitext(save_path)
+            saved_file = f'{partial_file[0]}_{i}{partial_file[1]}'
+            agg_df.to_csv(saved_file, sep='\t')
+            saved_files.append(saved_file)
+        else:
+            # Add to list of data
+            agg_list.append(agg_df)
+
+    print("All chunks processed")
+    if write_files:
+        if os.path.exists(save_path):
+            os.remove(save_path)
+        fout=open(save_path,"a")
+        # first file:
+        for line in open(saved_files[0]):
+            fout.write(line)
+        # now the rest:    
+        if len(saved_files) > 1:
+            for save_file in saved_files[1:]:
+                f = open(save_file)
+                for n, line in enumerate(f):
+                    if n != 0 and line not in ['', '\n']:
+                        fout.write(line)
+                f.close() # not really needed
+        fout.close()
+
+        agg_df = pd.read_csv(save_path, sep='\t', index_col='timestamp')
+        agg_df = agg_df.set_index(
+            pd.to_datetime(agg_df.index, format='%Y-%m-%d %H:%M:%S', errors='coerce')
+        )
+
+    else:
+        agg_df = pd.concat(agg_list)
 
     print("Filling All Time data")
     # Fill all the seconds between first and last second of data
@@ -156,22 +202,29 @@ def tweetsPreprocess(tweets_path, nrows=None):
         freq='min'
     )
 
+    df['no_tweets'] = df.iloc[:,0].isnull().astype('int8')
+    df = df.fillna(0)
+
     return df
 
 def pricesPreprocess(prices_path): 
 
     print("Loading raw file")
-    raw_df = pd.read_csv(prices_path, sep=',', index_col='Timestamp', parse_dates=True)
+    raw_df = pd.read_csv(
+        prices_path,
+        sep=',',
+        usecols=['Timestamp','Close', 'Volume_(BTC)'],
+        index_col='Timestamp',
+        parse_dates=True
+    )
     raw_df = raw_df.set_index(
         pd.to_datetime(raw_df.index, unit='s')
     )
 
-    df = raw_df[['Close', 'Volume_(BTC)']]
-
     print("Filling All Time data")
     # Fill all the seconds between first and last second of data
     df = fillAllTime(
-        df,
+        raw_df,
         freq='min'
     )
 
@@ -186,9 +239,10 @@ def pricesPreprocess(prices_path):
 %%time
 tweets_path = 'data/tweets_historical.csv'
 prices_path = 'data/bitstampUSD_1-min_data_2012-01-01_to_2019-08-12.csv'
-tweets_df = tweetsPreprocess(tweets_path, nrows=10000)
+tweets_df = tweetsPreprocess(tweets_path, nrows=1000, chunksize=400)
 prices_df = pricesPreprocess(prices_path)
 all_df = prices_df.merge(tweets_df, how='left', left_index=True, right_index=True)
+all_df['no_data'] = all_df[tweets_df.columns[0]].isnull()
 # %%
 %%time
 if __name__ == "__main__":
@@ -198,13 +252,11 @@ if __name__ == "__main__":
     print("Start tweetsPreprocess")
     tweets_df = tweetsPreprocess(tweets_path)
 
+    # TODO: Use Close - Close.shift()
     print("Start pricesPreprocess")
     prices_df = pricesPreprocess(prices_path)
 
     print("Joining prices and tweets")
     all_df = prices_df.merge(tweets_df, how='left', left_index=True, right_index=True)
-
-# %%
-
-
-# %%
+    all_df['no_data'] = all_df[tweets_df.columns[0]].isnull()
+    all_df.to_csv('data/all_data.csv', sep='\t')
