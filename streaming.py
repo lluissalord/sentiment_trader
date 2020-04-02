d0e8acf808eb558a9db5eebee13cbf8aead2f1a7
Lluis Salord Quetglas <l.salord.quetglas@gmail.com>
2020-04-02 10:50:47 +0200

    Improve documentation on streaming.py

--- streaming.py
+++ streaming.py
@@ -11,18 +11,8 @@ from utils import (
 )
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-import settings
 
-consumer_key = settings.consumer_key
-consumer_secret = settings.consumer_secret
-access_key = settings.access_key
-access_secret = settings.access_secret
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth)
-
-
+# Generalised function to extract text from tweets
 def getText(status):
     if hasattr(status, "retweeted_status"):  # Check if Retweet
         try:
@@ -39,13 +29,16 @@ def getText(status):
 
 
 class SentimentStreamListener(tweepy.StreamListener):
-
+    """
+    Listener extracting sentiment while streaming all tweets defined by filter
+    """
     def __init__(self):
         super().__init__()
         self.analyser = SentimentIntensityAnalyzer()
 
     def on_status(self, status):
 
+        # Data that will be stored directly from twitter
         data = [
             status.created_at,
             #status.user,
@@ -55,25 +48,28 @@ class SentimentStreamListener(tweepy.StreamListener):
             status.reply_count
         ]
 
+        # Extract text from tweet
         raw_text = getText(status)
 
-        # VADER
+        # VADER process
         text = vader_clean_tweets(raw_text)
         
+        # Compute VADER sentiment on text
         scores = self.analyser.polarity_scores(text)
         data += [scores['neg'], scores['neu'], scores['pos'], scores['compound']]
 
-        # TextBlob
+        # TextBlob process
         blob_text, blob = blob_clean_tweets(raw_text)
-        #pass textBlob method for sentiment calculations
+
+        # Pass textBlob method for sentiment calculations
         Sentiment = blob.sentiment
 
-        #seperate polarity and subjectivity in to two variables
+        # Seperate polarity and subjectivity in to two variables
         polarity = Sentiment.polarity
         subjectivity = Sentiment.subjectivity
-
         data += [polarity, subjectivity]
 
+        # TODO: Store data or get prediction from the model
         print(data)
 
 
@@ -89,8 +85,21 @@ class SentimentStreamListener(tweepy.StreamListener):
 # %%
 if __name__ == "__main__":
     
+    import settings
+
+    # Keys for twitter API authentification 
+    consumer_key = settings.consumer_key
+    consumer_secret = settings.consumer_secret
+    access_key = settings.access_key
+    access_secret = settings.access_secret
+
+    # Authentification process
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    # Create SentimentStreamListener 
     sapi = tweepy.streaming.Stream(auth, SentimentStreamListener())
+
+    # Apply filter of what should be listening
     sapi.filter(track=['bitcoin'], languages=['en'])
-
-
-# %%
