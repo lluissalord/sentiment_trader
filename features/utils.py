d7eae20f4db7c2201e7df65c103c6ffaee6f2d4d
Lluis Salord Quetglas <l.salord.quetglas@gmail.com>
2020-08-19 14:37:21 +0200

    Add normalize_data function

--- features/utils.py
+++ features/utils.py
@@ -4,6 +4,10 @@
 import numpy as np
 import pandas as pd
 
+from scipy.stats import normaltest
+from sklearn.preprocessing import PowerTransformer, QuantileTransformer, StandardScaler
+from unidip import UniDip
+
 import re
 import string
 
@@ -192,3 +196,47 @@ def fillAllTime(df, freq='min', on=None, keep='first', start_dt=None, end_dt=Non
         right_index=on is None,
         right_on=on,
     )
+
+
+def normalize_data(df, p_val_threshold=0.05, quantile_threshold=1000, max_quantiles=200, min_points_per_quantile=10):
+    """ Normalize to zero-mean and unit variance the continuos data using different transformation depending on the data distribution """
+    
+    fitted_df = df.copy()
+
+    # Define number of quantiles for the transformation
+    n_quantiles = min(len(df.index)//min_points_per_quantile, max_quantiles)
+
+    transformers = {}
+
+    # Calculate statistic normality tests for all float columns
+    float_cols = df.select_dtypes(include=[np.float]).columns
+    stats, p_values = normaltest(df[float_cols])
+    for i, column in enumerate(float_cols):
+        p_val = p_values[i]
+        # Determine if data follows a Gaussian distribution
+        if p_val < p_val_threshold:
+            # For large datasets is best to use directly Quantile even if meaning is lost
+            if len(df.index) > quantile_threshold:
+                # Use Quantile Transform for multimodal
+                transformer = QuantileTransformer(n_quantiles=n_quantiles, output_distribution='normal')
+            else:
+                try:
+                    # Test multimodality getting intervals of peaks
+                    intervals = UniDip(df[column], debug=False).run()
+
+                    # Only 1 peak means it is unimodal, otherwise multimodal 
+                    if len(intervals) == 1:
+                        transformer = PowerTransformer(method='yeo-johnson', standardize=True)
+                    else:
+                        # Use Quantile Transform for multimodal
+                        transformer = QuantileTransformer(n_quantiles=n_quantiles, output_distribution='normal')
+                except IndexError:
+                    transformer = PowerTransformer(method='yeo-johnson', standardize=True)
+        else:
+            # When data is following a Gaussian it is used a simple zero-mean unit variance transformation
+            transformer = StandardScaler(with_mean=True, with_std=True)
+
+        fitted_df[column] = transformer.fit_transform((df[column]).values.reshape(-1,1))
+        transformers[column] = transformer
+    
+    return fitted_df, transformer
